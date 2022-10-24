e2edf2b0d3b439d1402816d4fc0528cf966808c6
Si Yue Jiang <sj3834@nyu.edu>
2022-10-24 14:41:08 -0400

    setup code

---
+++ p5_midtermSketch4/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,800);
+  background(0);
+  
+}
+
+function draw() {
+  // put drawing code here
+  fill(117);
+  stroke(200,127,5);
+  strokeWeight(1);
+  rect(width/2 - 100, height/2 - 100, 200,200)
+}
